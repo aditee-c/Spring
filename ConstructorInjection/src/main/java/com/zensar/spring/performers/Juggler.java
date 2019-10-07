281fd332cd12f1517d07c42815d94f88cedb1a49
Aditee Chourasiya <aditee.chourasiya303@gmail.com>
2019-10-07 15:54:09 +0530

    Added Constructor Injection

---
+++ ConstructorInjection/src/main/java/com/zensar/spring/performers/Juggler.java
@@ -0,0 +1,21 @@
+package com.zensar.spring.performers;
+
+public class Juggler implements Performer {
+	private int beanBags;
+	public Juggler() {
+		System.out.println("No arg constructor of Juggler");
+	}
+	
+	public Juggler(int beanBags) {
+		super();
+		this.beanBags = beanBags;
+		System.out.println("Paramaterized constructor of Juggler");
+	}
+
+	@Override
+	public void perform() {
+		// TODO Auto-generated method stub
+		System.out.println("Juggler Juggling Bean Bags "+ beanBags);
+	}
+
+}
